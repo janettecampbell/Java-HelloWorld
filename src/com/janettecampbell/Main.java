34040d3d98aa1e12acd1843cfed57c158d7dc251
Janette Campbell <campbjp@hotmail.com>
2022-09-29 13:44:25 -0400

    chore: added date variable

--- src/com/janettecampbell/Main.java
+++ src/com/janettecampbell/Main.java
@@ -1,5 +1,6 @@
 package com.janettecampbell;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
@@ -10,10 +11,18 @@ public class Main {
         float price = 10.99F;
         char letter = 'A';
         boolean isEligible = false;
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
 
         Date now = new Date();
-        System.out.println(now);
 
+        System.out.println(point2);
+        System.out.println(now);
+        System.out.println(y);
         System.out.println(age);
         System.out.println("Hello World");
     }
