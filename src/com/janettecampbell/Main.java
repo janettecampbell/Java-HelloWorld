f2f270979f8afac210aef94277db93644062994f
Janette Campbell <campbjp@hotmail.com>
2022-09-29 15:25:55 -0400

    chore: added Implicit and Explicit Casting

--- src/com/janettecampbell/Main.java
+++ src/com/janettecampbell/Main.java
@@ -27,9 +27,30 @@ public class Main {
         int x1 = 1;
         x1 += 2;
 
+        // Implicit Casting
+        // double > float > long > int > short > byte
+        double x2 = 1.1;
+        double y2 = x2 + 2;
+
+        // Explicit Casting
+        double x3 = 1.1;
+        int y3 = (int) x3 + 2;
+
+        // String to number
+        String x4 = "1";
+        int y4 = Integer.parseInt(x4) + 2;
+        String x5 = "1.1";
+        double y5 = Double.parseDouble(x5) + 2;
+
+        // Math Class
+        
 
         Date now = new Date();
 
+        System.out.println(y5);
+        System.out.println(y4);
+        System.out.println(y3);
+        System.out.println(y2);
         System.out.println(x1);
         System.out.println(result);
         System.out.println(Arrays.deepToString(numbers));
