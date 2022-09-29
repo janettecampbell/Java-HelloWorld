1b98920673c3fe595a1d00155302f8a309b92ce4
Janette Campbell <campbjp@hotmail.com>
2022-09-29 11:48:38 -0400

    chore: added byte, long, float, char, and boolean type variables

--- src/com/janettecampbell/Main.java
+++ src/com/janettecampbell/Main.java
@@ -3,6 +3,13 @@ package com.janettecampbell;
 public class Main {
 
     public static void main(String[] args) {
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+        System.out.println(age);
         System.out.println("Hello World");
     }
 }
